d89135478240e4af7fd436b89dbc9f1608528844
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2015-11-17 23:25:08 +0900

    Add buildrequest tests.

--- test/server/Tests.hs
+++ test/server/Tests.hs
@@ -1,12 +1,14 @@
 module Server.Tests where
 
 import           Server.BuildQueueTests
+import           Server.BuildTests
 import           Server.CompilationTests
 import           Server.ReserveTests
 import           Test.Tasty
 
 serverTree = testGroup "Server tests" [
                 buildQueueTree,
+                buildTree,
                 compilationTree,
                 reserveTree 
               ]
